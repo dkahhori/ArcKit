2f98919d7e82bb14fcd6d50fffd992422cd2f3ac
Dilshodi Kahori <kahoridilshod@gmail.com>
2024-08-29 17:19:54 +0500

    Fixes

--- Sources/ArcKit/ArcKit.swift
+++ Sources/ArcKit/ArcKit.swift
@@ -1,34 +1,3 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import UIKit
-
-extension UIView {
-    /// Apply backgroundColor to a UIView
-    /// - Parameter color: UIColor for the background
-    func applyBackground(color: UIColor) {
-        self.backgroundColor = color
-    }
-
-    /// Apply shadow to a UIView
-    /// - Parameter color: UIColor of the shadow
-    /// - Parameter opacity: Transparency of the shadow
-    /// - Parameter radius: Radius of the shadow
-    /// - Parameter offset: Offset of the shadow
-    func applyShadow(color: UIColor = .darkGray, opacity: Float = 0.3, radius: CGFloat = 3.0, offset: CGSize = .zero) {
-        layer.shadowColor = color.cgColor
-        layer.shadowOpacity = opacity
-        layer.shadowRadius = radius
-        layer.shadowOffset = offset
-    }
-
-    /// Apply cornerRadius to a UIView
-    /// - Parameter radius: CGFloat of the corner
-    ///
-    /// - ATTENTION: By default clipsToBounds and cornerCurve = .continuous
-    func applyCorner(radius: CGFloat = 16) {
-        layer.cornerRadius = radius
-        layer.cornerCurve = .continuous
-        clipsToBounds = true
-    }
-}
