c491559210ce37b39d0d8c4b637bb25904b4357a
Dilshodi Kahori <kahoridilshod@gmail.com>
2024-08-29 18:02:08 +0500

    ArcLabel

---
+++ Sources/UILabel/ArcLabel.swift
@@ -0,0 +1,28 @@
+//
+//  ArcLabel.swift
+//
+//
+//  Created by Dilshodi Kahori on 29/08/24.
+//
+
+import UIKit
+
+open class ArcLabel: UILabel {
+    init(
+        text: String,
+        textColor: UIColor = .label,
+        alignment: NSTextAlignment,
+        numberOfLines: Int = 0
+    ) {
+        super.init(frame: .zero)
+        self.text = text
+        self.textColor = textColor
+        self.textAlignment = alignment
+        self.numberOfLines = numberOfLines
+        self.sizeToFit()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
