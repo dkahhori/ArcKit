2dbc6eb9442bb950eaf8f4effb8fe14a1b9abc3f
Dilshodi Kahori <kahoridilshod@gmail.com>
2024-08-29 17:33:48 +0500

    public access control

--- Sources/UIView+shadow/UIView+shadow.swift
+++ Sources/UIView+shadow/UIView+shadow.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIView {
     /// Apply backgroundColor to a UIView
     /// - Parameter color: UIColor for the background
-    func applyBackground(color: UIColor) {
+    public func applyBackground(color: UIColor) {
         self.backgroundColor = color
     }
 
@@ -19,7 +19,7 @@ extension UIView {
     /// - Parameter opacity: Transparency of the shadow
     /// - Parameter radius: Radius of the shadow
     /// - Parameter offset: Offset of the shadow
-    func applyShadow(color: UIColor = .darkGray, opacity: Float = 0.3, radius: CGFloat = 3.0, offset: CGSize = .zero) {
+    public func applyShadow(color: UIColor = .darkGray, opacity: Float = 0.3, radius: CGFloat = 3.0, offset: CGSize = .zero) {
         layer.shadowColor = color.cgColor
         layer.shadowOpacity = opacity
         layer.shadowRadius = radius
@@ -30,7 +30,7 @@ extension UIView {
     /// - Parameter radius: CGFloat of the corner
     ///
     /// - ATTENTION: By default clipsToBounds and cornerCurve = .continuous
-    func applyCorner(radius: CGFloat = 16) {
+    public func applyCorner(radius: CGFloat = 16) {
         layer.cornerRadius = radius
         layer.cornerCurve = .continuous
         clipsToBounds = true
