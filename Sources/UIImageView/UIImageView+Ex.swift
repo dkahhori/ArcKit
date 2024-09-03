f4d9ba306715647b383dd83b189216f1203f8ed3
Dilshodi Kahori <kahoridilshod@gmail.com>
2024-09-03 14:40:42 +0500

    UIImageView Extension

--- Sources/UIImageView/UIImageView+Ex.swift
+++ Sources/UIImageView/UIImageView+Ex.swift
@@ -5,4 +5,18 @@
 //  Created by Dilshodi Kahori on 03/09/24.
 //
 
-import Foundation
+import UIKit
+
+public extension UIImageView {
+    enum ImageSource {
+        case named(String)
+        case system(String)
+    }
+
+    func set(image source: ImageSource) {
+        switch source {
+        case .named(let imageName): self.image = UIImage(named: imageName)
+        case .system(let systemImageName): self.image = UIImage(systemName: systemImageName)
+        }
+    }
+}
